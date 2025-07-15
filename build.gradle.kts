f4a2fd72cc3807ed00b3b8c6d6f36e68abed9482
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-07-15 21:16:15 +0200

    chore: add initial project configuration files and CI workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,88 @@
 plugins {
-    id("java")
+    java
+    jacoco
+    `maven-publish`
 }
-
-group = "net.onelitefeather.vulpes"
-version = "1.0-SNAPSHOT"
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(platform(libs.mycelium.bom))
+    implementation(libs.minestom)
+    testImplementation(platform(libs.mycelium.bom))
+    testImplementation(libs.junit.api)
+    testImplementation(libs.junit.platform.launcher)
+    testRuntimeOnly(libs.junit.engine)
 }
 
-tasks.test {
-    useJUnitPlatform()
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+    withSourcesJar()
+    withJavadocJar()
+
+}
+
+tasks {
+    test {
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+    }
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+        version = rootProject.version as String
+        artifactId = "vulpes"
+        groupId = rootProject.group as String
+        pom {
+            name = "Vulpes"
+            description = "Vulpes for OneLiteFeather"
+            url = "https://github.com/OneLiteFeatherNET/vulpes"
+            licenses {
+                license {
+                    name = "AGPL-3.0"
+                    url = "https://www.gnu.org/licenses/agpl-3.0.en.html"
+                }
+            }
+            developers {
+                developer {
+                    id = "themeinerlp"
+                    name = "Phillipp Glanz"
+                    email = "p.glanz@madfix.me"
+                }
+                developer {
+                    id = "theEvilReaper"
+                    name = "Steffen Wonning"
+                    email = "steffenwx@gmail.com"
+                }
+                scm {
+                    connection = "scm:git:git://github.com:OneLiteFeatherNET/vulpes.git"
+                    developerConnection = "scm:git:ssh://git@github.com:OneLiteFeatherNET/vulpes.git"
+                    url = "https://github.com/OneLiteFeatherNET/vulpes"
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                authentication {
+                    credentials(PasswordCredentials::class) {
+                        // Those credentials need to be set under "Settings -> Secrets -> Actions" in your repository
+                        username = System.getenv("ONELITEFEATHER_MAVEN_USERNAME")
+                        password = System.getenv("ONELITEFEATHER_MAVEN_PASSWORD")
+                    }
+                }
+
+                name = "OneLiteFeatherRepository"
+                val releasesRepoUrl = uri("https://repo.onelitefeather.dev/onelitefeather-releases")
+                val snapshotsRepoUrl = uri("https://repo.onelitefeather.dev/onelitefeather-snapshots")
+                url = if (version.toString().contains("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            }
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,38 @@
 rootProject.name = "vulpes"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven {
+            name = "OneLiteFeatherRepository"
+            url = uri("https://repo.onelitefeather.dev/onelitefeather")
+            if (System.getenv("CI") != null) {
+                credentials {
+                    username = System.getenv("ONELITEFEATHER_MAVEN_USERNAME")
+                    password = System.getenv("ONELITEFEATHER_MAVEN_PASSWORD")
+                }
+            } else {
+                credentials(PasswordCredentials::class)
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
+            }
+        }
+    }
+
+    versionCatalogs {
+        create("libs") {
+            version("aonyx-bom", "0.5.0")
+            version("mycelium-bom", "1.4.0")
+
+            // Minestom
+            library("mycelium-bom", "net.onelitefeather", "mycelium-bom").versionRef("mycelium-bom")
+            library("minestom","net.minestom", "minestom").withoutVersion()
+
+            library("junit.api", "org.junit.jupiter", "junit-jupiter-api").withoutVersion()
+            library("junit.engine", "org.junit.jupiter", "junit-jupiter-engine").withoutVersion()
+            library("junit.params", "org.junit.jupiter", "junit-jupiter-params").withoutVersion()
+            library("junit.platform.launcher", "org.junit.platform", "junit-platform-launcher").withoutVersion()
+        }
+    }
+}
